9f0758c6ce23d8f73d78ccaf3db8e46be56d5f9f
Sarvesh-SP <sarvesh.sp2013@gmail.com>
2021-04-15 23:03:19 +0530

    Completed the menu

--- claci.py
+++ claci.py
@@ -16,5 +16,21 @@ while True:
   
   n1 = int(input('Enter the 1st Number: '))
   n2 = int(input('Enter the 2nd Number:'))
-
+  print('\n')
   print('-'*10 + 'MENU' + '-'*10)
+  print(''' 1.Addition(+)
+  2.Subtraction(-)
+  3.Multiplication(*)
+  4.Division(/)
+  5.Remainder(%)
+  ''')
+  
+  ch = input('Enter the symbol as your choice(+, -, *, /, %): ')
+
+  print('Result:' , claculate(n1, n2, ch))
+
+  exi = input('Wanna Enter More?(y/n)').lower()
+
+  if exi == 'n':
+    break
+
